c1d73af781ac8a1b6c932d7b4ff7fa84fecb8802
baranynah <a.kovalenko.ai@yandex.ru>
2023-01-09 18:50:07 +0300

    added default storages for each

--- encosy/storage/__init__.py
+++ encosy/storage/__init__.py
@@ -1,1 +1,4 @@
 from .meta import ResourceStorageMeta, SystemStorageMeta, EntityStorageMeta  # noqa
+from .resource_storage import SimpleResourceStorage as DefaultResourceStorage
+from .entity_storage import SimpleEntityStorage as DefaultEntityStorage
+from .system_storage import SimpleSystemStorage as DefaultSystemStorage
