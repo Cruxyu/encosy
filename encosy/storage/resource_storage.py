229667ea8c57512668f779cc89395e1d70d480c6
baranynah <a.kovalenko.ai@yandex.ru>
2023-01-09 18:27:24 +0300

    simple resource storage

--- encosy/storage/resource_storage.py
+++ encosy/storage/resource_storage.py
@@ -1,1 +1,20 @@
 from .meta import ResourceStorageMeta
+
+
+class SimpleResourceStorage(ResourceStorageMeta):
+    def __init__(self):
+        self.resources = {}
+
+    def add(self, resource):
+        self.resources[type(resource)] = resource
+        return self
+
+    def remove(self, resource: type):
+        self.resources.pop(resource)
+        return self
+
+    def query(self, *args, **kwargs):
+        pass
+
+    def get(self, resource: type):
+        return self.resources[resource]
