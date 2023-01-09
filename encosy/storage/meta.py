f23dc08d99e97857f08401d0450b3b4631e5db20
baranynah <a.kovalenko.ai@yandex.ru>
2023-01-09 16:18:37 +0300

    Update meta to use abstract method

--- encosy/storage/meta.py
+++ encosy/storage/meta.py
@@ -1,34 +1,43 @@
-from abc import ABCMeta
+from abc import ABCMeta, abstractmethod
 
 
 class EntityStorageMeta(metaclass=ABCMeta):
+    @abstractmethod
     def query(self, entity):
         pass
 
+    @abstractmethod
     def add(self, entity):
         pass
 
+    @abstractmethod
     def remove(self, entity):
         pass
 
 
 class ResourceStorageMeta(metaclass=ABCMeta):
+    @abstractmethod
     def query(self, entity):
         pass
 
+    @abstractmethod
     def add(self, entity):
         pass
 
+    @abstractmethod
     def remove(self, entity):
         pass
 
 
 class SystemStorageMeta(metaclass=ABCMeta):
+    @abstractmethod
     def query(self, entity):
         pass
 
+    @abstractmethod
     def add(self, entity):
         pass
 
+    @abstractmethod
     def remove(self, entity):
         pass
