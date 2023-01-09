7dc52e79f782565dcfa40de1f24c49d920d4a34b
baranynah <a.kovalenko.ai@yandex.ru>
2023-01-09 18:47:59 +0300

    simple storage for entities

--- encosy/storage/entity_storage.py
+++ encosy/storage/entity_storage.py
@@ -1,1 +1,37 @@
 from .meta import EntityStorageMeta
+from .typings import Entity, SystemConfig
+
+from typing import Any
+
+
+class SimpleEntityStorage(EntityStorageMeta):
+    def __init__(self):
+        self.entities = set()
+
+    def add(self, entity: Entity):
+        self.entities.add(entity)
+
+    def remove(self, entity: Entity):
+        self.entities.remove(entity)
+
+    def query(self, *args, **kwargs):
+        pass
+
+    def get(self, *types):
+        entities = []
+        components = set(types)
+        for entity in self.entities:
+            if components <= entity.keys():
+                entities.append(entity)
+        return entities
+
+    def query_expression(self, expression: ()):
+        entities = []
+        for entity in self.entities:
+            try:
+                expression(entity)
+            except KeyError:
+                continue
+            else:
+                entities.append(entity)
+        return entities
